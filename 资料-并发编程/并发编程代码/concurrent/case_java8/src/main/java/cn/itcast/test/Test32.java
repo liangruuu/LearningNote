7804e3aa7f20963efc4dc09fdaf77032859c337a
liangruuu <1024311844@qq.com>
2021-01-12 13:02:50 +0800

    update

--- 资料-并发编程/并发编程代码/concurrent/case_java8/src/main/java/cn/itcast/test/Test32.java
+++ 资料-并发编程/并发编程代码/concurrent/case_java8/src/main/java/cn/itcast/test/Test32.java
@@ -9,17 +9,24 @@ public class Test32 {
     // 易变
     static boolean run = true;
 
+    // 锁对象
+    final static Object lock = new Object();
+
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread(()->{
             while(true){
-                    if(!run) {
+                    synchronized (lock){
+                      if(!run){
                         break;
+                      }
                     }
             }
         });
         t.start();
 
         sleep(1);
-            run = false; // 线程t不会如预想的停下来
+        synchronized (lock){
+          run = false; // 线程t不会如预想的停下来
+        }
     }
 }
